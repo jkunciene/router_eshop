ba13aa325764f8bf54a2a4b5926e5c59f4356e65
jkunciene <j.kunciene@gmail.com>
2021-05-26 10:38:40 +0300

    prie namu darba

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -2,11 +2,16 @@ import React from 'react';
 
 import { Table } from 'react-bootstrap';
 
-const Cart = ({item}) => {
+const Cart = ({item, clearCart, deleteProduct, decreaseQuantity, increaseQuantity}) => {
+
+    const totalSum = () => {
+        return item.reduce((sum,{price, kiekis})=> sum + price*kiekis,0).toFixed(2);
+    }
     return (
         <div>
             <h2>Jusu krepselis</h2>
             <Table striped bordered hover>
+                {item.length > 0 &&
                 <thead>
                     <tr>
                     <th>#</th>
@@ -17,25 +22,30 @@ const Cart = ({item}) => {
                     <th>Trinti</th>
                     </tr>
                 </thead>
+                }
                 <tbody>
-                   {
+                   {                    
                         item && item.map((preke, index)=>(
                             <tr key={index}>
                                 <td>{index+1}</td>
-                                <td><img src={preke.imageUrl}/></td>
+                                <td><img className="img-thumbnail" style={{width: '90px'}} src={preke.imageUrl}/></td>
                                 <td>{preke.name}</td>
-                                <td><button>-</button>{preke.kiekis}<button>+</button></td>
+                                <td className="d-flex"><button className="btn btn-secondary mr-2" onClick={()=>decreaseQuantity(preke)}>-</button>{preke.kiekis}<button className="btn btn-secondary ml-2" onClick={()=>increaseQuantity(preke)}>+</button></td>
                                 <td>{preke.price}</td>
-                                <td><button>Trinti</button></td>
+                                <td><button className="btn btn-danger" onClick={()=>deleteProduct(preke)}>Trinti</button></td>
                             </tr>
-                        ))
-                   }
+                        ))   
+                        }  
+                {item.length > 0 &&          
                    <tr>
-                       <td colSpan="4">Bendra krepselio suma: </td>
-                       <td><button>Pirkti</button></td>
-                       <td><button>Salinti</button></td>
+                       <td colSpan="4">Bendra krepselio suma: {totalSum()} $</td>
+                       <td ><button className="btn btn-success" onClick={clearCart}>Pirkti</button></td>
+                       <td ><button className="btn btn-danger" onClick={clearCart}>Salinti</button></td>
                    </tr>
+                   }
                 </tbody>
+                    
+               
                 </Table>
         </div>
     )
--- src/components/Main.js
+++ src/components/Main.js
@@ -24,7 +24,7 @@ const Main = () => {
         let arYra = newCart.find((preke)=> product.name === preke.name);
         if(arYra && arYra.kiekis < product.countInStock){
             arYra.kiekis++;
-        } else if (arYra===undefined){
+        } else if (arYra===undefined && product.countInStock>0){
             arYra = {
                 ...product,
                 kiekis: 1
@@ -36,9 +36,45 @@ const Main = () => {
     setItem(newCart);
     }
     console.log(item)
+
+    //metodas krepselio isvalymui
+    const clearCart = ()=>{
+        setItem([]);
+    }
+    //konkrecios prekes trynimo  metodas
+    const deleteProduct = (product) => {
+        console.log(product);
+        setItem(item.filter((p) => p !== product));
+    }
+    //konkrecios prekes kiekio mazinimas
+    const decreaseQuantity = (product) => {
+        console.log("kiekio mazinimas "+product)
+        let newCart = [...item];
+        let arYra = newCart.find((preke)=> product.name === preke.name);
+        if(arYra.kiekis > 1){
+            arYra.kiekis--;
+            setItem(newCart.splice(arYra));
+        } else {
+            setItem(item.filter((p) => p !== arYra))
+        }
+    }
+
+    //konkrecios prekes kiekio didinimas
+    const increaseQuantity = (product)=>{
+         
+        let newCart = [...item];
+        let arYra = newCart.find((preke)=> product.name === preke.name);
+        if(arYra && arYra.kiekis < product.countInStock){
+            arYra.kiekis++;
+        } else {
+            alert("Deja prekes sandelyje jau nebera")
+        }
+        setItem(newCart)
+    }
+
     return (
         <Router>
-            <Navbar bg="light" variant="light">
+            <Navbar bg="light" variant="light" className="sticky-top">
                  <Link to="/"><Navbar.Brand>e-Shop</Navbar.Brand></Link>
                     <Nav className="mr-auto">
                         <Nav.Link><Link to="/">Products</Link></Nav.Link>
@@ -47,7 +83,7 @@ const Main = () => {
             </Navbar>
             <Switch>
                 <Route path="/cart">
-                    <Cart item={item}/>
+                    <Cart item={item} clearCart={clearCart} deleteProduct={deleteProduct} decreaseQuantity={decreaseQuantity} increaseQuantity={increaseQuantity}/>
                 </Route>
                 <Route path="/">
                     <Products addToCart={addToCart}/>
